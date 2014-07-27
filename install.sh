7756969f6e81e8094e5b11febed1559d3004a2cb
ashraf8 <ashraf8@gmail.com>
2014-07-27 11:12:04 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,6 @@ echo "Installing Dot files"
 echo "Installing Screenrc file"
 ln -s ~/.dotfiles/.screenrc ~/.screenrc
 
+echo "Installing bash aliases"
+ln -s ~/.dotfiles/.bash_aliases ~/.bash_aliases
 echo "Done"

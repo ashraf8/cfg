ad50872e8eb85b046096dc4045b566c4816192db
ashraf8@gmail.com <user@smtp.gmail.com:587>
2015-03-09 07:40:22 +0100

    adding tmux config file

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@ echo "Installing Dot files"
 echo "Installing Screenrc file"
 ln -s $(pwd)/.screenrc ~/.screenrc
 
+echo "Installing tmux file"
+ln -s $(pwd)/.tmux.conf ~/.tmux.conf
+
 echo "Installing bash aliases"
 ln -s $(pwd)/.bash_aliases ~/.bash_aliases
 echo "Done"
